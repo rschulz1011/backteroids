cc7f8cc57e723b69fc8c9cde93722dbef290b093
unknown <ryandschulz@gmail.com>
2015-04-07 00:56:19 -0500

    partial commit of difficulty setting UI

--- levelSelect.js
+++ levelSelect.js
@@ -3,6 +3,8 @@ var levelSelect = function(game){
 	levelTileSize = 70;
 	levelTileMargin = 30;
 	selectedLevel = null;
+	difficultyLevels = {};
+	currentLevel = level;
 };
 
 levelSelect.prototype = {
@@ -11,6 +13,8 @@ levelSelect.prototype = {
 		game.load.spritesheet('levelButton', 'res/levelButton.png',70,70);
 		game.load.image('playButton','res/playButton.png');
 		game.load.spritesheet('muteButton','res/mute_button.png',30,20);
+		game.load.spritesheet('increaseDifficulty','res/increase_difficulty.png',25,25);
+		game.load.spritesheet('decreaseDifficulty','res/decrease_difficulty.png',25,25);
 
 		loadShipSprites();
 		preLoadButtons();
@@ -35,7 +39,8 @@ levelSelect.prototype = {
 		});
 		buttons = this.game.add.group();
 
-		playButton = this.game.add.button(20,400,"playButton",clickPlayButton,this);
+		playButton = this.game.add.button((game.width - 6*levelTileSize - levelTileMargin)/2,475,"playButton",clickPlayButton,this);
+		playButton.anchor.x = 0.5;
 		playButton.kill();
 
 		var selectLevelStyle = { font: "18px Arial", stroke: "bold", fill: "#eeeeee"};
@@ -51,6 +56,10 @@ levelSelect.prototype = {
 
 		displayTotalScore();
 		createNavigationButtons("LevelSelect");
+
+		difficultyLevelControls = game.add.group();
+		addDifficultyLevelControls(level);
+		difficultyLevelControls.alpha = 0;
 	},
 }
 
@@ -113,6 +122,8 @@ var clickLevelButton = function(button) {
 
 var populateLevelInfo = function(level) {
 
+	currentLevel = level;
+
 	if (level===null)
 	{
 		levelInfoNameText = game.add.text((game.width - 6*levelTileSize - levelTileMargin)/2,65," ",levelInfoNameStyle);
@@ -123,6 +134,11 @@ var populateLevelInfo = function(level) {
 		drawLevelParameter("bestScore","Best Score: ",170);
 		drawLevelParameter("shipTypes","Ship Types: ",200);
 		shipTypes = game.add.group();
+
+		var difficultyLevelStyle = { font: "18px Arial", stroke: "bold", fill: "#eeeeee"};
+		var difficultyLevelText = game.add.text((game.width - 6*levelTileSize - levelTileMargin)/2,250,"Difficulty Settings",difficultyLevelStyle);
+		difficultyLevelText.anchor.x = 0.5;
+		
 	}
 	else
 	{
@@ -149,8 +165,113 @@ var populateLevelInfo = function(level) {
 		{
 			levelInfoValue["bestScore"].setText("--");
 		}
+
+		resetDifficultyLevels();
+
+		
+		if (playerData.levelScores[level] !== undefined && playerData.levelScores[level].passed)
+		{
+			difficultyLevelControls.alpha = 1;
+		}
+		else
+		{
+			difficultyLevelControls.alpha = 0.3;
+		}
+		
+		updateDifficultyControls(level);
+
 	}
 };
+
+var addDifficultyLevelControls = function(level) {
+
+	difficultySettingName = {};
+	difficultySettingValue = {};
+	difficultySettingMultiplier = {};
+	difficultyDecreaseButton = {};
+	difficultyIncreaseButton = {};
+
+	var ypos = 280;
+
+	$.each(difficultySetting,function(index, control) {
+		addDifficultyControl(index,control,level,ypos);
+		ypos = ypos +33;
+	})
+
+	updateDifficultyControls(level);
+	
+};
+
+var addDifficultyControl = function(index,control,level,ypos) {
+
+	var difficultySettingNameStyle = {font: "14px Arial", fill: "#FFFFFF"};
+	difficultySettingName[index] = game.add.text(97,ypos-3,control.name,difficultySettingNameStyle);
+	difficultySettingName[index].anchor.x = 0.5;
+	difficultyLevelControls.add(difficultySettingName[index]);
+
+	var difficultySettingValueStyle = {font: "bold 14px Arial", fill: "#FFFF00"};
+	difficultySettingValue[index] = game.add.text(97,ypos+12,control.displayString(2),difficultySettingValueStyle);
+	difficultySettingValue[index].anchor.x = 0.5;
+	difficultyLevelControls.add(difficultySettingValue[index]);
+
+	var difficultySettingMultiplierStyle = {font: "18px Arial", fill: "#FFFF00"};
+	difficultySettingMultiplier[index] = game.add.text(200,ypos+12,"x "+control.multiplier(2).toFixed(2),difficultySettingMultiplierStyle);
+	difficultySettingMultiplier[index].anchor.y = 0.5;
+	difficultyLevelControls.add(difficultySettingMultiplier[index]);
+
+	difficultyDecreaseButton[index] = this.game.add.button(20,ypos,"decreaseDifficulty",difficultyButtonClick,this,1,0,0,0);
+	difficultyDecreaseButton[index].name = index;
+	difficultyDecreaseButton[index].direction = -1;
+	difficultyIncreaseButton[index] = this.game.add.button(150,ypos,"increaseDifficulty",difficultyButtonClick,this,1,0,0,0);
+	difficultyIncreaseButton[index].name = index;
+	difficultyIncreaseButton[index].direction = 1;
+	difficultyLevelControls.add(difficultyDecreaseButton[index]);
+	difficultyLevelControls.add(difficultyIncreaseButton[index]);
+
+	
+}
+
+var difficultyButtonClick = function(button) {
+	var direction = 0;
+	difficultyLevels[button.name] = difficultyLevels[button.name] + button.direction;
+	updateDifficultyControls(currentLevel);
+};
+
+var resetDifficultyLevels = function() {
+	$.each(difficultySetting,function(index){
+		difficultyLevels[index] = 0;
+	});
+};
+
+var updateDifficultyControls = function(level) {
+	$.each(difficultySetting,function(index,control){
+		difficultySettingValue[index].text = control.displayString(difficultyLevels[index]);
+		difficultySettingMultiplier[index].text = "x "+control.multiplier(difficultyLevels[index]).toFixed(2);
+
+		if (difficultyLevels[index]<=0)
+		{
+			difficultyDecreaseButton[index].inputEnabled = false;
+			setTimeout(function(){difficultyDecreaseButton[index].frame = 2;},10);
+		}
+		else
+		{
+			difficultyDecreaseButton[index].inputEnabled = true;
+			difficultyDecreaseButton[index].frame = 0;
+		}
+
+		if (level === null || difficultyLevels[index]>=control.maxLevel ||  playerData.levelScores[level] === undefined || !playerData.levelScores[level].passed )
+		{
+			difficultyIncreaseButton[index].inputEnabled = false;
+			setTimeout(function(){difficultyIncreaseButton[index].frame=2;},10);
+		}
+		else
+		{
+			difficultyIncreaseButton[index].inputEnabled = true;
+			difficultyIncreaseButton[index].frame = 0;
+		}
+
+	});
+}
 
 var calculateBasePoints = function(level){
 
@@ -161,7 +282,7 @@ var calculateBasePoints = function(level){
 		});
 	});
 	return totalBasePoints;
-}
+};
 
 var getUniqueShipTypes = function(level){
 
@@ -192,3 +313,33 @@ var drawLevelParameter = function(parameterId, parameterTitle,ypos) {
 	levelInfoValue[parameterId] = game.add.text(180,ypos," ",levelValueStyle);
 	levelInfoValue[parameterId].anchor.x = 0;
 }
+
+
+var difficultySetting = {
+	shipSpeed: {
+		name: "Ship Speed",
+		value: function(level) {
+			return level*10;
+		},
+		displayString: function(level) {
+			return "+"+level*10+"%";
+		},
+		multiplier: function(level) {
+			return 1 + level/20;
+		},
+		maxLevel: 20,
+	},
+	fireRate: {
+		name: "Fire Rate",
+		value: function(level) {
+			return level*10;
+		},
+		displayString: function(level) {
+			return "+"+level*10+"%";
+		},
+		multiplier: function(level) {
+			return 1 + level/10;
+		},
+		maxLevel: 20,
+	}
+};
