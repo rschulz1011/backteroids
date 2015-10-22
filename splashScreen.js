e3f616e2110fad7364dc4827610bc77dc31bd86e
unknown <ryandschulz@gmail.com>
2015-10-22 18:40:14 -0500

    Added splash screen graphics

--- splashScreen.js
+++ splashScreen.js
@@ -4,13 +4,17 @@ splashScreen.prototype = {
 
 	preload: function() {
 		game.load.image('playButton','res/playButton.png');
+		game.load.image('splash','res/splash.png')
 		game.load.spritesheet('muteButton','res/mute_button.png',30,20);
 		preLoadSounds();
 	},
 
 	create: function() {
 		addMuteButton(765,30);
-		var playButton = this.game.add.button(400,300,'playButton',this.playButtonClick,this);
+
+		this.game.add.sprite(0,0,'splash');
+
+		var playButton = this.game.add.button(375,400,'playButton',this.playButtonClick,this);
 		playButton.anchor.setTo(0.5,0.5);
 	},
 
