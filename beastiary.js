673ceff99a2518569e53f1554da4d54a279a98f7
rschulz1011 <ryandschulz@gmail.com>
2018-02-03 21:59:32 -0500

    initial commit of Beastiary screen including grid for ships

--- beastiary.js
+++ beastiary.js
@@ -10,12 +10,44 @@ beastiary.prototype = {
 	create: function() {
 		addMuteButton(765,30);
 		createNavigationButtons("Beastiary");
-		thisText = game.add.text(400,300,"Coming Soon!",{fill:"#ffffff"});
-		thisText.anchor.x = 0.5;
-		thisText.anchor.y = 0.5;
+
+		topText = game.add.text(400,55,"Beastiary",{fill:"#ffffff"});
+		topText.anchor.x = 0.5;
+		topText.anchor.y = 0.5;
+
+		drawBeastiaryEntry(165,90,0);
 
 		displayTotalScore();
 		calculatePlayerLevel(gameData);
 		displayPlayerLevel();
+
+		$.getJSON( "gameData.json", function( data ) {
+			gameData = data;
+			shipTypeIndex = 0;
+			$.each(gameData.shipTypes,function(index,shipType) {
+				var tileWidth = 70;
+				xid = shipTypeIndex % 7;
+				yid = Math.floor(shipTypeIndex/7);
+				drawBeastiaryEntry(155+xid*tileWidth,90+yid*tileWidth,shipType);
+				shipTypeIndex++;
+			});
+		})
+		.fail(function(jqxhr, textStatus, error){
+			var err = textStatus + ", " + error;
+		});
 	},
+}
+
+function drawBeastiaryEntry(x,y,ship)
+{
+	var tileWidth = 70;
+	var graphics = game.add.graphics(0, 0);
+	//draw box around achievement
+	graphics.lineStyle(1, 0x222222, 1);
+	graphics.beginFill(0x050505, 1);
+    graphics.drawRect(x,y,70,70);
+
+    shipTitleText = game.add.text(x+tileWidth/2,y+10,ship.name,{font: "10px Arial",fill:"#ffffff"});
+    shipTitleText.anchor.x = 0.5;
+    shipTitleText.anchor.y = 0.5;
 }
