ad118103766c1fdc6b6c16585214dd7ededc3d3c
rschulz1011 <ryandschulz@gmail.com>
2018-02-09 20:13:25 -0500

    adding beastiary functionality including ship display

--- beastiary.js
+++ beastiary.js
@@ -4,6 +4,7 @@ beastiary.prototype = {
 
 	preload: function() {
 		game.load.spritesheet('muteButton','res/mute_button.png',30,20);
+		game.load.image('unknown','res/QuestionMark.png');
 		preLoadButtons();
 	},
 
@@ -18,6 +19,14 @@ beastiary.prototype = {
 		displayTotalScore();
 		calculatePlayerLevel(gameData);
 		displayPlayerLevel();
+
+		ships = game.add.group();
+		ships.enableBody = true;
+		ships.allowRotation = true;
+
+		tiles = game.add.group();
+
+		graphics = game.add.graphics(0, 0);
 
 		$.getJSON( "gameData.json", function( data ) {
 			gameData = data;
@@ -34,22 +43,67 @@ beastiary.prototype = {
 			var err = textStatus + ", " + error;
 		});
 	},
+
+	update:  function () {}
 }
 
 function drawBeastiaryEntry(x,y,ship,id)
 {
 	var tileWidth = 70;
-	var graphics = game.add.graphics(0, 0);
-	//draw box around achievement
-	graphics.lineStyle(1, 0x222222, 1);
-	graphics.beginFill(0x050505, 1);
-    graphics.drawRect(x,y,70,70);
 
-    shipTitleText = game.add.text(x+tileWidth/2,y+12,ship.name,{font: "10px Arial",fill:"#ffffff"});
-    shipTitleText.anchor.x = 0.5;
-    shipTitleText.anchor.y = 0.5;
+	var bmd = game.add.bitmapData(tileWidth,tileWidth);
 
-    killsText = game.add.text(x+tileWidth/2,y+tileWidth-4,"Kills: "+playerData.playerStats.shipStats[id].kills,{font: "9px Arial",fill:"#999999"});
-    killsText.anchor.x = 0.5;
-    killsText.anchor.y = 0.5;
+   	bmd.ctx.beginPath();
+   	bmd.ctx.rect(0,0,tileWidth,tileWidth);
+   	bmd.ctx.fillStyle = '#050505';
+   	bmd.ctx.lineStyle = '#222222';
+   	bmd.ctx.fill();
+
+   	var tileSprite = tiles.create(x,y,bmd);
+
+   	if (playerData.playerStats.shipStats[id].encountered > 0) {
+
+    	shipTitleText = game.add.text(x+tileWidth/2,y+12,ship.name,{font: "10px Arial",fill:"#ffffff"});
+    	shipTitleText.anchor.x = 0.5;
+    	shipTitleText.anchor.y = 0.5;
+
+    	killsText = game.add.text(x+tileWidth/2,y+tileWidth-4,"Kills: "+playerData.playerStats.shipStats[id].kills,{font: "9px Arial",fill:"#999999"});
+    	killsText.anchor.x = 0.5;
+    	killsText.anchor.y = 0.5;
+
+    	var shipSprite = ships.create(tileWidth/2,tileWidth/2,ship.imageKey);
+    	shipSprite.anchor.x = 0.5;
+    	shipSprite.anchor.y = 0.5;
+    	shipSprite.angle = Math.random() * 360;
+
+    	tileSprite.addChild(shipSprite);
+    	shipSprite.bringToTop();
+
+    	var maxShipSize = Math.max(shipSprite.width,shipSprite.height);
+
+    	if (maxShipSize > 42) {
+    		shipSprite.width = shipSprite.width * 42/maxShipSize;
+    		shipSprite.height = shipSprite.height * 42/maxShipSize;
+    	}
+
+    	if (playerData.playerStats.shipStats[id].kills > 0)
+    	{
+    		shipSprite.body.angularVelocity = Math.random() * 10 - 5;
+    	}
+    	else
+    	{
+    		shipSprite.alpha = 0.15;
+    		shipTitleText.alpha = 0.25;
+    		killsText.alpha = 0.25;
+    	}
+	} 
+	else 
+	{
+		var unknownSprite = game.add.sprite(tileWidth/2,tileWidth/2,'unknown');
+		unknownSprite.anchor.x = 0.5;
+		unknownSprite.anchor.y = 0.5;
+		tileSprite.addChild(unknownSprite);
+	}
+
+
 }
